openwrt/make_ext4fs

commit 5c201be7d72aff735da27e17c29852e0cefe3e52

Add LDFLAGS when building libsparse.a
Respect the LDFALGS provided from the outside when linking libsparse.a

Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

Makefile
@@ -27,7 +27,7 @@ OBJ := \
 	$(CC) $(CFLAGS) -c -o $@ $^
 
 make_ext4fs: $(OBJ) libsparse/libsparse.a
-	$(CC) -o $@ $^ $(ZLIB)
+	$(CC) $(LDFLAGS)-o $@ $^ $(ZLIB)
 
 libsparse/libsparse.a:
 	$(MAKE) -C libsparse/ libsparse.a

commit 13767a96fcdffd59fef16f1b86919f63b6dee45a

make_ext4fs: add missing space after LDFLAGS
If LDFLAGS is non-empty then next standing "-o" flag may be mistakenly
appended to it leading to build error.

Fixes: 5c201be7d72a ("Add LDFLAGS when building libsparse.a")
Signed-off-by: Konstantin Demin <rockdrilla@gmail.com>
[ improve commit description ]
Link: https://github.com/openwrt/make_ext4fs/pull/3
Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

Makefile
@@ -27,7 +27,7 @@ OBJ := \
 	$(CC) $(CFLAGS) -c -o $@ $^
 
 make_ext4fs: $(OBJ) libsparse/libsparse.a
-	$(CC) $(LDFLAGS)-o $@ $^ $(ZLIB)
+	$(CC) $(LDFLAGS) -o $@ $^ $(ZLIB)
 
 libsparse/libsparse.a:
 	$(MAKE) -C libsparse/ libsparse.a